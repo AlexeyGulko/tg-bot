b7cd9a994f1007fef44e05b71b7e74b9affd3ab6
Aleksey Gulko <a.gulko@sredaobuchenia.ru>
2022-09-26 22:16:39 +0300

    base

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"gitlab.ozon.dev/dev.gulkoalexey/gulko-alexey/internal/clients/tg"
+	"gitlab.ozon.dev/dev.gulkoalexey/gulko-alexey/internal/config"
+	"gitlab.ozon.dev/dev.gulkoalexey/gulko-alexey/internal/model/messages"
+	"log"
+)
+
+func main() {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
---
+++ internal/model/messages/incoming_msg.go
@@ -0,0 +1,27 @@
+package messages
+
+type MessageSender interface {
+	SendMessage(text string, userId int64) error
+}
+
+type Model struct {
+	tgClient MessageSender
+}
+
+func New(tgClient MessageSender) *Model {
+	return &Model{tgClient}
+}
+
+type Message struct {
+	Text   string
+	UserID int64
+}
+
+func (s Model) IncomingMessage(msg Message) error {
+	if msg.Text == "/start" {
+		s.tgClient.SendMessage("hello", msg.UserID)
+		return nil
+	}
+	s.tgClient.SendMessage("не знаю эту команду", msg.UserID)
+	return nil
+}
